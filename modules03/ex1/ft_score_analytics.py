88dd47bdf856bb320b3e7087d83f22ed0e09fa99
Fodé Manca <mancafode11@gmail.com>
2026-01-16 03:16:43 +0000

    doing manual loops insted of using append

--- modules03/ex1/ft_score_analytics.py
+++ modules03/ex1/ft_score_analytics.py
@@ -2,29 +2,26 @@
 
 import sys
 
+def score_analytics():
+    print("=== Player Score Analytics ===\n")
+    args = sys.argv[1:]
+    if len(args) < 1:
+        print("No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+        return
 
-def Score_Analytics():
-    print("=== player Score Analytics ===\n")
-    argc = len(sys.argv) - 1
-    if argc < 1:
-        print(
-            "No scores provided. Usage: python3 ft_score_analytics.py"
-            "<score1> <score2 ...>")
-    else:
-        try:
-            score = []
-            for arg in sys.argv[1:]:
-                score.append(int(arg))
-            print(f"Scores processed: {score}")
-            print(f"Total players: {len(score)}")
-            print(f"Total score: {sum(score)}")
-            print(f"Average score: {sum(score) / len(score):.1f}")
-            print(f"High score: {max(score)}")
-            print(f"Low score: {min(score)}")
-            print(f"Score range: {max(score) - min(score)}")
-        except ValueError:
-            print("erro try again")
-
+    try:
+        score = []
+        for arg in args:
+            score += [int(arg)]
+        print(f"Scores processed: {score}")
+        print(f"Total players: {len(score)}")
+        print(f"Total score: {sum(score)}")
+        print(f"Average score: {sum(score) / len(score):.1f}") 
+        print(f"High score: {max(score)}")
+        print(f"Low score: {min(score)}")
+        print(f"Score range: {max(score) - min(score)}")
+    except ValueError:
+        print("Error: Please provide valid integer scores.")
 
 if __name__ == "__main__":
-    Score_Analytics()
+    score_analytics()
