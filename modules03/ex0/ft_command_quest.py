3d49f3555f52eedea23eb89ce9e76e1854e5453d
Fode Manca <fomanca@c2r5s11.42porto.com>
2026-01-18 19:17:18 +0000

    Implement CLI argument processor using sys.argv for basic input handling

--- modules03/ex0/ft_command_quest.py
+++ modules03/ex0/ft_command_quest.py
@@ -4,14 +4,16 @@ import sys
 
 if __name__ == "__main__":
     print("=== Command Quest ===")
-    argc = len(sys.argv)
+    """
+    Basic command-line argument processor
+    """
+    argc: int = len(sys.argv)
     if argc < 2:
         print("No arguments provided!")
     print(f"Program name: {sys.argv[0]}")
     if argc >= 2:
         print(f"Arguments received: {argc - 1}")
-        j = 1
-        while j < argc:
-            print(f"Argument {j}: {sys.argv[j]}")
-            j += 1
+        for i in range(1, argc):
+            print(f"Argument {i}: {sys.argv[i]}")
+
     print(f"Total arguments: {argc}")
